c7a22276a632e61d2f04e29e61945334c393785f
0xWafar <155046671+wafardev@users.noreply.github.com>
2023-12-31 02:39:33 +0100

    Merkle Tree Verification Added

--- client/index.js
+++ client/index.js
@@ -1,17 +1,30 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+const verifyProof = require("../utils/verifyProof");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // fetch the name from the command line
+  // no name in the command line args, no verification
+  if (process.argv.length < 3)
+    console.log("Please provide a name for verification.");
+  else {
+    // if name in multiple words, they are joined
+    const name = process.argv.slice(2).join(" ");
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+    const merkleTree = new MerkleTree(niceList);
+    const proof = merkleTree.getProof(niceList.findIndex((n) => n === name));
 
-  console.log({ gift });
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      // send the proof and name to the server
+      name,
+      proof,
+    });
+
+    console.log({ gift });
+  }
 }
 
 main();
